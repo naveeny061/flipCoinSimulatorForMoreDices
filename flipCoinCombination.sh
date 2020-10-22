d1594a66601fe5441f08bec0cfd5c9105c4980d2
naveeny061 <naveeny061@gmail.com>
2020-10-22 15:46:10 +0530

    adding doublet code

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,23 +1,38 @@
 #!/bin/bash -x
 declare -A coinResult
-heads=0
-tails=0
+HH=0
+TT=0
+HT=0
+TH=0
 echo "Welcome to the Flip Coin Combination Program"
 loop=0
 while [ $loop -lt 10 ]
 do
-randomValue=$((RANDOM%2))
-if [[ $randomValue -eq 0 ]]
+randomValue1=$((RANDOM%2))
+randomValue2=$((RANDOM%2))
+if [[ $randomValue1 -eq 0 && $randomValue2 -eq 0 ]]
 then
-((heads++))
-coinResult[0]=$heads
-echo "heads"
+((HH++))
+coinResult[HH]=$HH
+echo "HH"
+elif [[ $randomValue1 -eq 1 && $randomValue2 -eq 1 ]]
+then
+((TT++))
+coinResult[TT]=$TT
+echo "TT"
+elif [[ $randomValue1 -eq 0 && $randomValue2 -eq 1 ]]
+then
+((HT++))
+coinResult[HT]=$HT
+echo "HT"
 else
-((tails++))
-coinResult[1]=$tails
-echo "tails"
+((TH++))
+coinResult[TH]=$TH
+echo "TH"
 fi
 ((loop++))
 done
-echo "heads percentage is " $((heads*10))
-echo "tails parcentage is " $((tails*10))
+echo "HH percentage is " $((HH*10))
+echo "TT parcentage is " $((TT*10))
+echo "HT percentage is " $((HT*10))
+echo "TH parcentage is " $((TH*10))
